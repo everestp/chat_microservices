e2e0abb885c2289b47bc43c0d5e88093e3cec49e
everestp <paudeleverest09@gmail.com>
2026-02-24 10:43:54 +0545

    Setup RabbitMQ

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -2,8 +2,11 @@ import { configDotenv } from 'dotenv';
 import express from 'express'
 import connectDB from './config/db.js';
 import {createClient} from "redis"
+import userRouter from './routes/user.js';
+import connectRabbitMQ from './config/rabbitmq.js';
 configDotenv()
 connectDB()
+connectRabbitMQ()
 
 export const redisClient =  createClient({
     url:process.env.REDIS_URL,
@@ -13,6 +16,8 @@ redisClient.connect()
 .catch(()=>console.log("connected to redis"))
 const app = express();
 const port = process.env.PORT;
+app.use("api/v1",userRouter)
+
 app.listen(port , ()=>{
     console.log("Server is running")
 })
